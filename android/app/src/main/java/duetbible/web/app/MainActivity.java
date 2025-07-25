cdbd2d9585bd5f4110e6e14622a00066c32cb5f7
SaucaVictor <victorclashroyale72@example.com>
2025-07-25 04:14:07 +0300

    android capacitor

---
+++ android/app/src/main/java/duetbible/web/app/MainActivity.java
@@ -0,0 +1,5 @@
+package duetbible.web.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'duetbible.web.app',
+  appName: 'duet.bible',
+  webDir: 'dist'
+};
+
+export default config;
